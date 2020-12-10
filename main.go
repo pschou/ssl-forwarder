22f8d3e7d46a905b8b84e38ef7d9ee89493e2eb1
schou <github@paulschou.com>
2020-12-10 09:47:03 -0500

    add load all CAs

--- main.go
+++ main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"crypto/rand"
 	"crypto/tls"
+	"crypto/x509"
+	"encoding/pem"
 	"flag"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"net"
 	"os"
@@ -20,6 +23,7 @@ type DNS struct {
 var target_addr = ""
 var DNSCache = make(map[string]DNS, 0)
 var keypair tls.Certificate
+var rootpool *x509.CertPool
 
 func main() {
 	flag.Usage = func() {
@@ -30,6 +34,7 @@ func main() {
 	var target = flag.String("target", "127.0.0.1:443", "Sending address for forwarder")
 	var cert = flag.String("cert", "/etc/pki/server.pem", "File to load with CERT")
 	var key = flag.String("key", "/etc/pki/server.pem", "File to load with KEY")
+	var root = flag.String("ca", "/etc/pki/ca-trust/extracted/pem/tls-ca-bundle.pem", "File to load with ROOT CAs")
 	flag.Parse()
 
 	target_addr = *target
@@ -37,9 +42,13 @@ func main() {
 	var err error
 	keypair, err = tls.LoadX509KeyPair(*cert, *key)
 	if err != nil {
-		log.Fatalf("server: loadkeys: %s", err)
+		log.Fatalf("failed to loadkeys: %s", err)
 	}
-	config := tls.Config{Certificates: []tls.Certificate{keypair}}
+	rootpool, err = LoadCertficatesFromFile(*root)
+	if err != nil {
+		log.Fatalf("failed to load CA: %s", err)
+	}
+	config := tls.Config{Certificates: []tls.Certificate{keypair}, RootCAs: rootpool}
 	config.Rand = rand.Reader
 	if l, err = tls.Listen("tcp", *listen, &config); err != nil {
 		log.Fatal(err)
@@ -54,7 +63,7 @@ func main() {
 
 		go func(c net.Conn) {
 			defer c.Close()
-			config := tls.Config{Certificates: []tls.Certificate{keypair}, InsecureSkipVerify: true}
+			config := tls.Config{Certificates: []tls.Certificate{keypair}, RootCAs: rootpool}
 			remote, err := tls.Dial("tcp", target_addr, &config)
 			if err != nil {
 				log.Println("dialing endpoint:", target_addr, "error:", err)
@@ -65,4 +74,25 @@ func main() {
 			io.Copy(c, remote)
 		}(conn)
 	}
+}
+
+func LoadCertficatesFromFile(path string) (*x509.CertPool, error) {
+	pool := x509.NewCertPool()
+	raw, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	for {
+		block, rest := pem.Decode(raw)
+		if block == nil {
+			break
+		}
+		if block.Type == "CERTIFICATE" {
+			pool.AppendCertsFromPEM(block.Bytes)
+		}
+		raw = rest
+	}
+
+	return pool, nil
 }
