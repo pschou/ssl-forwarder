7ce54934151eecc1063927cf88a0bfcf2661dc6a
schou <github@paulschou.com>
2020-12-11 19:47:24 -0500

    reworking the design to implement automatic certificate rotation

--- main.go
+++ main.go
@@ -1,3 +1,9 @@
+//
+//  This package was written by Paul Schou in Dec 2020
+//
+//  Originally intended to help with linking two apps together and expanded to be a general
+//    FOSS software for use to link apps together that usually don't do mTLS (mutual TLS)
+//
 package main
 
 import (
@@ -12,6 +18,7 @@ import (
 	"log"
 	"net"
 	"os"
+	"sync"
 	"time"
 )
 
@@ -22,42 +29,125 @@ type DNS struct {
 
 var target_addr = ""
 var DNSCache = make(map[string]DNS, 0)
-var keypair tls.Certificate
+var keyFile = ""
+var certFile = ""
+var keypair *tls.Certificate
+var keypair_count = 0
+var keypair_mu sync.RWMutex
+var rootFile = ""
+var root_count = 0
 var rootpool *x509.CertPool
+var certs_loaded = make(map[string]bool, 0)
 var debug = false
+
+func loadKeys() {
+	keypair_mu.RLock()
+	defer keypair_mu.RUnlock()
+	var err error
+
+	tmp_key, err_k := tls.LoadX509KeyPair(certFile, keyFile)
+	if err_k != nil {
+		if keypair == nil {
+			log.Fatalf("failed to loadkey pair: %s", err)
+		}
+		keypair_count++
+		log.Println("WARNING: Cannot load keypair (cert/key)", certFile, keyFile, "attempt:", keypair_count)
+		if keypair_count > 10 {
+			log.Fatalf("failed to loadkey pair: %s", err)
+		}
+	} else {
+		if debug {
+			log.Println("Loaded keypair", certFile, keyFile)
+		}
+		keypair = &tmp_key
+		keypair_count = 0
+	}
+
+	err_r := LoadCertficatesFromFile(rootFile)
+	if err_r != nil {
+		if rootpool == nil {
+			log.Fatalf("failed to load CA: %s", err)
+		}
+		root_count++
+		log.Println("WARNING: Cannot load CA file", rootFile, "attempt:", root_count)
+		if root_count > 10 {
+			log.Fatalf("failed to CA: %s", err)
+		}
+	} else {
+		if debug {
+			log.Println("Loaded CA", rootFile)
+		}
+		root_count = 0
+	}
+
+}
 
 func main() {
 	flag.Usage = func() {
-		fmt.Fprintf(os.Stderr, "Very simple SSL forwarder, written by Paul Schou github@paulschou.com\n\n Usage of %s:\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "Very simple SSL forwarder, written by Paul Schou github@paulschou.com in December 2020\nIf you find this useful, please hit me up and let me know.\n\n Usage of %s:\n", os.Args[0])
 		flag.PrintDefaults()
 	}
 	var listen = flag.String("listen", ":7443", "Listen address for forwarder")
 	var target = flag.String("target", "127.0.0.1:443", "Sending address for forwarder")
-	var cert_file = flag.String("cert", "/etc/pki/server.pem", "File to load with CERT")
-	var key_file = flag.String("key", "/etc/pki/server.pem", "File to load with KEY")
-	var root = flag.String("ca", "/etc/pki/ca-trust/extracted/pem/tls-ca-bundle.pem", "File to load with ROOT CAs")
+	var cert_file = flag.String("cert", "/etc/pki/server.pem", "File to load with CERT - automatically reloaded every minute")
+	var key_file = flag.String("key", "/etc/pki/server.pem", "File to load with KEY - automatically reloaded every minute")
+	var root_file = flag.String("ca", "/etc/pki/ca-trust/extracted/pem/tls-ca-bundle.pem", "File to load with ROOT CAs - reloaded every minute by adding any new entries")
+	var verify_client = flag.Bool("verify-client", true, "Verify or disable client certificate check")
+	var verify_server = flag.Bool("verify-server", true, "Verify or disable server certificate check")
+	var secure_client = flag.Bool("secure-client", true, "Enforce TLS 1.2 on client side")
+	var secure_server = flag.Bool("secure-server", true, "Enforce TLS 1.2 on server side")
 	var tls_enabled = flag.Bool("tls", true, "Enable listener TLS")
-	var tls_verify = flag.Bool("tls_verify", true, "Verify TLS or disable all checks")
 	var tls_host = flag.String("host", "", "Hostname to verify outgoing connection with")
-	var verbose = flag.Bool("v", false, "Verbose output")
+	var verbose = flag.Bool("debug", false, "Verbose output")
 	flag.Parse()
 
 	var err error
 	debug = *verbose
-	rootpool, err = LoadCertficatesFromFile(*root)
-	if err != nil {
-		log.Fatalf("failed to load CA: %s", err)
-	}
 
-	keypair, err = tls.LoadX509KeyPair(*cert_file, *key_file)
-	if err != nil {
-		log.Fatalf("failed to loadkey pair: %s", err)
-	}
+	keyFile = *key_file
+	certFile = *cert_file
+	rootFile = *root_file
+	rootpool = x509.NewCertPool()
+
+	loadKeys()
+	go func() {
+		ticker := time.NewTicker(time.Minute)
+		for {
+			select {
+			case <-ticker.C:
+				loadKeys()
+			}
+		}
+	}()
 
 	var l net.Listener
 	if *tls_enabled {
-		config := tls.Config{Certificates: []tls.Certificate{keypair}, RootCAs: rootpool,
-			ClientCAs: rootpool, InsecureSkipVerify: *tls_verify == false, ServerName: *tls_host}
+		var config tls.Config
+		if *secure_client {
+			config = tls.Config{RootCAs: rootpool,
+				Certificates: []tls.Certificate{},
+				ClientCAs:    rootpool, InsecureSkipVerify: *verify_client == false,
+				MinVersion:               tls.VersionTLS12,
+				CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
+				PreferServerCipherSuites: true,
+				CipherSuites: []uint16{
+					tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+					tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+					tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+					tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+				},
+			}
+		} else {
+			config = tls.Config{RootCAs: rootpool,
+				ClientCAs: rootpool, InsecureSkipVerify: *verify_client == false}
+		}
+		config.GetCertificate = func(clientHello *tls.ClientHelloInfo) (*tls.Certificate, error) {
+			if debug {
+				log.Println("  Get Cert Returning keypair")
+			}
+			return keypair, nil
+		}
+
 		config.Rand = rand.Reader
 		if debug {
 			fmt.Println("TLS Listening on", *listen)
@@ -84,17 +174,45 @@ func main() {
 	for {
 		conn, err := l.Accept() // Wait for a connection.
 		if err != nil {
+			fmt.Println("Error on accept", err)
 			continue
+		}
+		if debug {
+			fmt.Println("New connection from", conn.RemoteAddr())
 		}
 
 		go func(c net.Conn) {
+			defer conn.Close()
 			defer c.Close()
-			config := tls.Config{Certificates: []tls.Certificate{keypair}, RootCAs: rootpool,
-				ClientCAs: rootpool, InsecureSkipVerify: *tls_verify == false, ServerName: *tls_host}
-			remote, err := tls.Dial("tcp", target_addr, &config)
+			var tlsConfig *tls.Config
+			if *secure_server {
+				tlsConfig = &tls.Config{Certificates: []tls.Certificate{*keypair}, RootCAs: rootpool,
+					ClientCAs: rootpool, InsecureSkipVerify: *verify_client == false, ServerName: *tls_host,
+					MinVersion:               tls.VersionTLS12,
+					CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
+					PreferServerCipherSuites: true,
+					CipherSuites: []uint16{
+						tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+						tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+						tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+						tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+					},
+				}
+			} else {
+				tlsConfig = &tls.Config{Certificates: []tls.Certificate{*keypair}, RootCAs: rootpool,
+					ClientCAs: rootpool, InsecureSkipVerify: *verify_server == false, ServerName: *tls_host}
+			}
+
+			if debug {
+				log.Println("dialing endpoint:", target_addr)
+			}
+			remote, err := tls.Dial("tcp", target_addr, tlsConfig)
 			if err != nil {
-				log.Println("dialing endpoint:", target_addr, "error:", err)
+				log.Println("error dialing endpoint:", target_addr, "error:", err)
 				return
+			}
+			if debug {
+				log.Println("connected!", target_addr)
 			}
 
 			go io.Copy(remote, c)
@@ -103,16 +221,12 @@ func main() {
 	}
 }
 
-func LoadCertficatesFromFile(path string) (*x509.CertPool, error) {
+func LoadCertficatesFromFile(path string) error {
 	raw, err := ioutil.ReadFile(path)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	pool := x509.NewCertPool()
-	if debug {
-		fmt.Println("Loading CA certs...")
-	}
 	for {
 		block, rest := pem.Decode(raw)
 		if block == nil {
@@ -124,13 +238,17 @@ func LoadCertficatesFromFile(path string) (*x509.CertPool, error) {
 				fmt.Println("warning: error parsing CA cert", err)
 				continue
 			}
-			if debug {
-				fmt.Println(" ", cert.Subject)
+			t := fmt.Sprintf("%v%v", cert.SerialNumber, cert.Subject)
+			if _, ok := certs_loaded[t]; !ok {
+				if debug {
+					fmt.Println(" Adding CA:", cert.Subject)
+				}
+				rootpool.AddCert(cert)
+				certs_loaded[t] = true
 			}
-			pool.AddCert(cert)
 		}
 		raw = rest
 	}
 
-	return pool, nil
+	return nil
 }
